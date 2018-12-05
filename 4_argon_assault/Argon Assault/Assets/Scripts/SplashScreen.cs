06c54fa8cb1227674d7b420c03fbfcd659313757
Rodrigo Sanchez <rodrifs@gmail.com>
2018-12-05 20:11:03 +0100

    feat: add terrain, splash scene, music, skybox and ship

---
+++ 4_argon_assault/Argon Assault/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+	void Awake () {
+		DontDestroyOnLoad (this.gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+		Invoke ("LoadFirstLevel", 2f);
+	}
+
+	private void LoadFirstLevel () {
+		SceneManager.LoadScene (1);
+	}
+}
